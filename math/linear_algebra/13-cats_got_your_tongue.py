712cebcfbe51d6c0b8521932a7730de69000ee95
Ganzadidier <d.ganza@alustudent.com>
2025-05-24 05:00:06 -0700

    2 last files

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,18 +1,38 @@
 #!/usr/bin/env python3
-"""Concatenates two matrices along a specified axis."""
+"""
+Concatenates two matrices along a specified axis using NumPy.
+"""
 
 import numpy as np
 
+
 def np_cat(mat1, mat2, axis=0):
     """
-    Concatenates two matrices along a specific axis.
+    Concatenates two NumPy matrices (arrays) along a specified axis.
+
+    This function wraps NumPy's concatenate method to join two arrays
+    along the given axis. Both arrays must have the same shape, except
+    in the dimension corresponding to the specified axis.
 
     Args:
-        mat1 (np.ndarray): The first matrix.
-        mat2 (np.ndarray): The second matrix.
-        axis (int): The axis along which to concatenate.
+        mat1 (np.ndarray): The first matrix to concatenate.
+        mat2 (np.ndarray): The second matrix to concatenate.
+        axis (int): The axis along which the matrices will be concatenated.
+                    Must be a valid axis for the given arrays.
 
     Returns:
         np.ndarray: The concatenated matrix.
+
+    Raises:
+        ValueError: If the arrays cannot be concatenated along the given axis.
+
+    Example:
+        np_cat(np.array([[1, 2]]), np.array([[3, 4]]), axis=0)
+        -> array([[1, 2],
+                  [3, 4]])
+
+        np_cat(np.array([[1], [2]]), np.array([[3], [4]]), axis=1)
+        -> array([[1, 3],
+                  [2, 4]])
     """
     return np.concatenate((mat1, mat2), axis=axis)
--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-"""Performs matrix multiplication using NumPy."""
+"""
+Performs matrix multiplication using NumPy.
+"""
 
 import numpy as np
 
+
 def np_matmul(mat1, mat2):
     """
-    Performs matrix multiplication between two numpy ndarrays.
+    Performs matrix multiplication between two NumPy ndarrays.
 
     Args:
         mat1 (np.ndarray): First input matrix.
@@ -13,5 +16,9 @@ def np_matmul(mat1, mat2):
 
     Returns:
         np.ndarray: Result of the matrix multiplication.
+
+    Example:
+        np_matmul(np.array([[1, 2]]), np.array([[3], [4]]))
+        -> array([[11]])
     """
     return np.matmul(mat1, mat2)
