979d9c40514297c8160647993f164622daa5fc3b
Ganzadidier <d.ganza@alustudent.com>
2025-05-29 04:20:35 -0700

    changing the logic

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -19,9 +19,8 @@ def determinant(matrix):
     """
     # Validate input is a list of lists
     if (not isinstance(matrix, list) or
-	 not all(isinstance(row, list) for row in matrix)):
-		raise TypeError("matrix must be a list of lists")
-
+            not all(isinstance(row, list) for row in matrix)):
+        raise TypeError("matrix must be a list of lists")
 
     # Validate matrix is square
     n = len(matrix)
@@ -36,13 +35,15 @@ def determinant(matrix):
 
     # Base case: 2x2 matrix
     if n == 2:
-        return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
+        return (matrix[0][0] * matrix[1][1] -
+                matrix[0][1] * matrix[1][0])
 
     # Recursive case for nxn matrix
     det = 0
     for col in range(n):
-        # Get minor matrix
-        minor = [row[:col] + row[col+1:] for row in matrix[1:]]
-        # Recursive call and cofactor expansion
+        minor = [
+            row[:col] + row[col + 1:]
+            for row in matrix[1:]
+        ]
         det += ((-1) ** col) * matrix[0][col] * determinant(minor)
     return det
